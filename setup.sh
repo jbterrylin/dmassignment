daa5049043e8f6f4b43af16c7df81699d48bbd3a
Jbterrylin <wanglinlee1707@gmail.com>
2021-09-24 08:16:17 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"1171200748@student.mmu.edu.my\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
